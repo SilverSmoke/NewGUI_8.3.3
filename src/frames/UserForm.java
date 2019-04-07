71f3ee7d9f1d071f51448804ce06187525788fb8
andrei_belikov <omgumail@yandex.ru>
2019-04-07 12:19:45 +0600

    Test1commit

---
+++ src/frames/UserForm.java
@@ -0,0 +1,33 @@
+package frames;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class UserForm {
+    private JPanel rootPanel;
+    private JTextField surName;
+    private JTextField patronymic;
+    private JTextField name;
+    private JTextField cityOfResidence;
+    private JFormattedTextField birthday;
+    private JButton save;
+
+    public UserForm() {
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(name.getText()==""){
+                    JOptionPane.showMessageDialog(rootPanel,"Введите имя!");
+                }
+                if(surName.getText()==""){
+                    JOptionPane.showMessageDialog(rootPanel,"Введите фамилию!");
+                }
+            }
+        });
+    }
+
+    public JPanel getRootPanel() {
+        return rootPanel;
+    }
+}
---
+++ src/main/Main.java
@@ -0,0 +1,22 @@
+package main;
+
+import frames.UserForm;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+
+        UserForm form = new UserForm();
+        frame.setContentPane(form.getRootPanel());
+
+        frame.setTitle("GUI Basics");
+        frame.setSize(800, 600);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
